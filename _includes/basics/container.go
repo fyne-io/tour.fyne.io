13ee4169eedbe50990620209a995e022db00ad0b
Andy Williams <andy@andy.xyz>
2019-06-10 22:26:10 +0100

    A better window name

--- _includes/basics/container.go
+++ _includes/basics/container.go
@@ -11,15 +11,15 @@ import (
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Canvas")
+	myWindow := myApp.NewWindow("Container")
 
 	text1 := canvas.NewText("Hello", color.White)
 	text2 := canvas.NewText("There", color.White)
 	text2.Move(fyne.NewPos(20, 20))
 	text3 := canvas.NewText("World", color.White)
 	container := fyne.NewContainer(text1, text2, text3)
-//	container := fyne.NewContainerWithLayout(layout.NewGridLayout(2),
-//		text1, text2, text3)
+	//	container := fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+	//		text1, text2, text3)
 
 	myWindow.SetContent(container)
 	myWindow.ShowAndRun()
